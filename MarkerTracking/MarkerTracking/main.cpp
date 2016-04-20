d3d1d1ca6d4f5c776c62addb1eb5e7aa96aa20db
Philipp Müller <philipp_mueller1991@yahoo.de>
2016-04-20 03:49:39 +0200

    Small extensions

--- MarkerTracking/MarkerTracking/main.cpp
+++ MarkerTracking/MarkerTracking/main.cpp
@@ -17,6 +17,7 @@ int main(int arc, char** argv)
 
 	// open window and attach trackbar
 	cv::namedWindow("camera", 1);
+	cv::namedWindow("debug", 1);
 	cv::namedWindow("result", 1);
 	int threshold = 50;
 	cv::createTrackbar("threshold", "camera", &threshold, 255, nullptr);
@@ -34,12 +35,22 @@ int main(int arc, char** argv)
 		cv::cvtColor(frame, image, CV_RGB2GRAY);
 		cv::threshold(image, image, threshold, 255, cv::THRESH_BINARY);
 
+		// find contours and restrict number of endpoints
+		cv::Mat debugImg = image.clone();
+		std::vector<std::vector<cv::Point>> contours;
+		cv::findContours(image, contours, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
+
+		// convert debug image back to color space and draw debug lines
+		cv::cvtColor(image, image, CV_GRAY2BGR);
+		cv::drawContours(image, contours, 0, cv::Scalar(0, 0, 250, 1), 2);
+
 		// show images
 		cv::imshow("camera", frame);
+		cv::imshow("debug", debugImg);
 		cv::imshow("result", image);
 		
 		// pull key
-		lastKey = cv::waitKey(10);
+		lastKey = cv::waitKey(1);
 	}
 
 	cap.release();	// Release VideoCapture
