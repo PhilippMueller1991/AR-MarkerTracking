f87ff1bbe5d178243f077a4169b9b10904662d08
Philipp Müller <philipp_mueller1991@yahoo.de>
2016-05-24 16:22:30 +0200

    Exercise 2 done

--- MarkerTracking/MarkerTracking/main.cpp
+++ MarkerTracking/MarkerTracking/main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
+// opencv headers
 #include <opencv2\opencv.hpp>
 
 #define KEY_ESC 27
+
+
+void initVideoStream(cv::VideoCapture &cap) {
+	if (cap.isOpened())
+		cap.release();
+
+	cap.open(0); // open the default camera
+	if (cap.isOpened() == false) {
+		std::cout << "No webcam found, using a video file" << std::endl;
+		cap.open("MarkerMovie.mpg");
+		if (cap.isOpened() == false) {
+			std::cout << "No video file found. Exiting." << std::endl;
+			exit(0);
+		}
+	}
+}
 
 int main(int arc, char** argv)
 {
 	std::cout << "OpenCV version: " << CV_VERSION << std::endl;
 
-	cv::VideoCapture cap(0);
+	cv::VideoCapture cap;
+	initVideoStream(cap);
 
 	if (!cap.isOpened())
 	{
@@ -15,45 +33,87 @@ int main(int arc, char** argv)
 		return EXIT_SUCCESS;
 	}
 
-	// open window and attach trackbar
-	cv::namedWindow("camera", 1);
+	// Create window and attach trackbar
 	cv::namedWindow("debug", 1);
 	cv::namedWindow("result", 1);
-	int threshold = 50;
-	cv::createTrackbar("threshold", "camera", &threshold, 255, nullptr);
+	int sliderValue = 50;
+	cv::createTrackbar("threshold", "debug", &sliderValue, 255);
 
-	// main loop
-	int lastKey = -1;
-	cv::Mat image;
-	while (lastKey != KEY_ESC)
+	// MAIN LOOP
+	std::vector<std::vector<cv::Point>> contours;
+	std::vector<cv::Point> rect;
+	CvMemStorage* memStorage = cvCreateMemStorage();
+	// Images
+	cv::Mat imgColor;
+	cv::Mat imgGray;
+	cv::Mat imgFiltered;
+	while (true)
 	{
-		// get camera frame
-		cv::Mat frame;
-		cap >> frame;
+		// Get camera frame
+		cap >> imgColor;
 
-		// convert image to black and white and threshold
-		cv::cvtColor(frame, image, CV_RGB2GRAY);
-		cv::threshold(image, image, threshold, 255, cv::THRESH_BINARY);
+		if (imgColor.empty())
+		{
+			std::cout << "Could not query frame. Trying to reinitialize." << std::endl;
+			initVideoStream(cap);
+			cv::waitKey(1000);
+			continue;
+		}
 
-		// find contours and restrict number of endpoints
-		cv::Mat debugImg = image.clone();
-		std::vector<std::vector<cv::Point>> contours;
-		cv::findContours(image, contours, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
+		// Convert image to black and white and threshold
+		cv::cvtColor(imgColor, imgGray, CV_BGR2GRAY);
+		cv::threshold(imgGray, imgFiltered, sliderValue, 255, cv::THRESH_BINARY);
+		cv::imshow("debug", imgFiltered);
 
-		// convert debug image back to color space and draw debug lines
-		cv::cvtColor(image, image, CV_GRAY2BGR);
-		cv::drawContours(image, contours, 0, cv::Scalar(0, 0, 250, 1), 2);
-
-		// show images
-		cv::imshow("camera", frame);
-		cv::imshow("debug", debugImg);
-		cv::imshow("result", image);
+		// Find contours (old OpenCV API)
+		CvSeq* contours;
+		CvMat imgFilteredCpy(imgFiltered);
+		cvFindContours(&imgFilteredCpy, memStorage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
 		
-		// pull key
-		lastKey = cv::waitKey(1);
+		// For each header in contours we approximate a polygon and count the edges
+		for (; contours; contours = contours->h_next)
+		{
+			// Approximate polygons
+			CvSeq* polys = cvApproxPoly(contours, sizeof(CvContour), memStorage, CV_POLY_APPROX_DP, cvContourPerimeter(contours) * 0.02, 0);
+
+			if (polys->total != 4)
+				continue;
+
+			cv::Mat result = cv::cvarrToMat(polys);
+			cv::Rect r = cv::boundingRect(result);
+
+			// Discard rects that are too small or too big
+			if(r.height<20 || r.width<20 || r.height > imgFiltered.rows -10 || r.width > imgFiltered.cols - 10)
+				continue;
+
+			const cv::Point* rect = (const cv::Point*) result.data;
+			int numPoints = result.rows;
+
+			cv::polylines(imgColor, &rect, &numPoints, 1, true, CV_RGB(255,0,0), 2);
+
+			for (int i = 0; i < 4; i++)
+			{
+				cv::Point2d dir = cv::Point2d(rect[(i + 1) % 4] - rect[i]) / 7.0;
+				for (int j = 1; j < 7; j++)
+				{
+					cv::Point2d pos = cv::Point2d(rect[i]) + j * dir;
+					cv::circle(imgColor, pos, 2, CV_RGB(0, 0, 255), -1);
+				}
+				cv::circle(imgColor, rect[i], 3, CV_RGB(0, 255, 0), -1);
+			}
+		}
+		
+		cv::imshow("result", imgColor);
+		// Clear
+		cvClearMemStorage(memStorage);
+		// Poll keys
+		if(cv::waitKey(10) == KEY_ESC) break;
 	}
 
+	cvReleaseMemStorage(&memStorage);	// Release contours memory storage
 	cap.release();	// Release VideoCapture
 	
+	cv::destroyAllWindows();
+
 	return EXIT_SUCCESS;
 }
